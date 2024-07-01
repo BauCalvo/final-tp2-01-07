fc1ec4ee6dfbe1c4f0a54028841b423cf194cedc
baucalvo <baucalvo@gmail.com>
2024-07-01 20:59:34 -0300

    cambio de formato

--- Utils/dateUtils.js
+++ Utils/dateUtils.js
@@ -1,7 +1,7 @@
 function getFechaActual() {
     const now = new Date();
     const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // Los meses son de 0 a 11
+    const month = String(now.getMonth() + 1).padStart(2, '0');
     const day = String(now.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
   }
--- controllers/SondaController.js
+++ controllers/SondaController.js
@@ -11,7 +11,9 @@ class SondaController {
     create = async (req, res) => {
         try {
             const { id, temperatura } = req.body;
-
+            if (!id || !temperatura) {
+                throw new Error("Datos no válidos")
+            }
 
             const fecha = dateUtils.getFechaActual();
             const hora = dateUtils.getHoraActual();
@@ -21,32 +23,32 @@ class SondaController {
                 fecha: fecha,
                 hora: hora
             };
+
             const data = await this.sondaApi.create(info)
-            res.status(200).send(data)
+            res.status(200).send({ message: "Sonda creada", sonda: data })
         }
         catch (error) {
-            res.status(422).send(error.message)
+            res.status(422).send({message:{message:error.message}})
         }
     }
 
     getAll = async (req, res) => {
         try {
             const data = await this.sondaApi.getAll()
-            res.status(200).send(data)
+            res.status(200).send({sondas:data})
         }
         catch (error) {
-            res.status(422).send(error.message)
+            res.status(422).send({message:error.message})
         }
     }
     getById = async (req, res) => {
         try {
             const { id } = req.params;
-            
             const data = await this.sondaApi.getById(id)
-            res.status(200).send(data)
+            res.status(200).send({sonda:id,datos:data})
         }
         catch (error) {
-            res.status(422).send(error.message)
+            res.status(422).send({message:error.message})
         }
     }
 
